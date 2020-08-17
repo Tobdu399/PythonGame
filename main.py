7409258ea0f6858370673d46936c2345d6eba0a4
Tobdu399 <topias.niiranen12@gmail.com>
2020-08-17 17:57:50 +0300

    Files Encrypted

---
+++ main.py
@@ -0,0 +1,11 @@
+import sourcedefender
+import settings as gs
+import controls
+import obstacle
+from game import game
+
+#! The game starts only if this file is executed
+if __name__ == '__main__':
+    game.show_game()
+    with gs.Listener(on_press=controls.on_press, on_release=controls.on_release) as listener:
+        listener.join()
